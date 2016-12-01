653abdb0158cc7d34b4b673199bd26b6dd7b8979
AmiReza71 <amir.winfa@gmail.com>
2016-12-01 19:57:54 +0330

    Update ShortLink.php

--- ShortLink.php
+++ ShortLink.php
@@ -1,5 +1,5 @@
 <?php
-define('API_KEY', 'TOKEN');
+define('API_KEY', '293671742:AAHHxDBKm8QJrdWq-5NktTSA0MPqYiy05jk');
 $admin = "ADMIN";
 function mute($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;

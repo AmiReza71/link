54298d14810b4c33e1012eeea34989c66baf58c8
AmiReza71 <amir.winfa@gmail.com>
2016-12-01 20:39:24 +0330

    Update and rename ShortLink.php to Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,6 +1,6 @@
 <?php
 define('API_KEY', '308659671:AAG8VKiQkwVhCheUsGIQpNe0aGP2LOEsEM8');
-$admin = 195472744;
+$admin = "ADMIN";
 function mute($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
     $ch = curl_init();
